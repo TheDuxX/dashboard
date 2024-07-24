6a4b20e3d57369ab038f35794b2194590735f330
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2024-07-24 17:24:01 -0300

    feat: add middleware

--- app/(home)/_components/header.tsx
+++ app/(home)/_components/header.tsx
@@ -1,20 +1,30 @@
+"use client"
 import { Avatar } from "@/app/_components/ui/avatar";
 import Image from "next/image";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
-import { getUser } from "@/auth/server";
-import { redirect } from "next/navigation";
+import { useState } from "react";
+import { createSupabaseClient } from "@/auth/client";
+import { User } from "@supabase/supabase-js";
+
+
 
 const wordlLink = " de ";
 
-const Header = async () => {
-  const user = await getUser();
+const Header = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const { auth } = createSupabaseClient();
+
+  auth.onAuthStateChange((event, session) => {
+    setUser(session?.user || null)
+  })
 
   return (
     <>
       {user ? (<div className="w-full min-h-[75px] bg-white rounded-md p-2 flex justify-between items-center shadow">
         <div className="flex flex-col gap-1">
-          <h2 className="font-medium text-lg">Olá, usuário</h2>
+          <h2 className="font-medium text-lg">Olá, {user?.email || "Não está logado"}</h2>
           <div className="flex gap-1 text-sm">
             <p className="capitalize">
               {format(new Date(), "EEEE', 'd", {
@@ -32,7 +42,7 @@ const Header = async () => {
         <Avatar className="relative border border-solid min-h-[50px] min-w-[50px]">
           <Image src="./vercel.svg" alt="avatar" fill className="object-fill" />
         </Avatar>
-      </div>) : redirect("/login")}
+      </div>) : ""}
     </>
   );
 };
