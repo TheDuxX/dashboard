10a778ee6aa67a36d416890ddee320bd149f73f0
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2024-07-25 11:52:29 -0300

    feat: remove supabase auth

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,9 @@
-import ProductList from "../_components/list";
-import SignOutButton from "../_components/sign-out-button";
-import Header from "./_components/header";
 import MostViews from "./_components/Most-views";
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col gap-2">
-        {/* <Header /> */}
         <MostViews />
       </div>
     </>
