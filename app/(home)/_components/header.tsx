901118c2171650fcb9ae2046eac8c540516050fc
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2024-07-25 13:23:03 -0300

    feat: reset home page and header component

--- app/(home)/_components/header.tsx
+++ app/(home)/_components/header.tsx
@@ -1,30 +1,20 @@
-"use client"
+"use client";
 import { Avatar } from "@/app/_components/ui/avatar";
 import Image from "next/image";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import { useState } from "react";
-import { createSupabaseClient } from "@/auth/client";
-import { User } from "@supabase/supabase-js";
-
-
 
 const wordlLink = " de ";
 
 const Header = () => {
-  const [user, setUser] = useState<User | null>(null);
-
-  const { auth } = createSupabaseClient();
-
-  auth.onAuthStateChange((event, session) => {
-    setUser(session?.user || null)
-  })
-
   return (
     <>
-      {user ? (<div className="w-full min-h-[75px] bg-white rounded-md p-2 flex justify-between items-center shadow">
+      <div className="w-full min-h-[75px] bg-white rounded-md p-2 flex justify-between items-center shadow">
         <div className="flex flex-col gap-1">
-          <h2 className="font-medium text-lg">Olá, {user?.email || "Não está logado"}</h2>
+          <h2 className="font-medium text-lg">
+            Olá, "Não está logado"
+          </h2>
           <div className="flex gap-1 text-sm">
             <p className="capitalize">
               {format(new Date(), "EEEE', 'd", {
@@ -42,7 +32,7 @@ const Header = () => {
         <Avatar className="relative border border-solid min-h-[50px] min-w-[50px]">
           <Image src="./vercel.svg" alt="avatar" fill className="object-fill" />
         </Avatar>
-      </div>) : ""}
+      </div>
     </>
   );
 };
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,11 @@
+import Header from "./_components/header";
 import MostViews from "./_components/Most-views";
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col gap-2">
+        <Header />
         <MostViews />
       </div>
     </>
