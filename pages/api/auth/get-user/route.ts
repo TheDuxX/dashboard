f7f621a780b088ad8f3259f67d88a69a5c31e016
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2024-07-30 16:38:49 -0300

    Feat: change location

--- pages/api/auth/get-user/route.ts
+++
@@ -1,9 +0,0 @@
-import { getUser } from "@/supabase/auth/server";
-
-export async function GET(req: Request) {
-  const user = await getUser();
-
-  return new Response(JSON.stringify(user), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
