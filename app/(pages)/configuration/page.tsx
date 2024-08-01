9942bd8229312f2aa26af1b4f64e68a5ab01f27e
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2024-08-01 16:55:12 -0300

    feat: clean configuration page

--- app/(pages)/configuration/page.tsx
+++ app/(pages)/configuration/page.tsx
@@ -42,32 +42,11 @@ const Config = () => {
           </TabsContent>
           <TabsContent value="test">
             <Card className="rounded-t-none">
-              <CardContent className="p-0 py-2 min-h-20">
-                
-              </CardContent>
+              <CardContent className="p-0 py-2 min-h-20"></CardContent>
             </Card>
           </TabsContent>
         </Tabs>
       </div>
-
-      {/* <div className="flex flex-row gap-2">
-        <Link href="/configuration/profile" className="w-1/2 aspect-square">
-          <Button
-            className="w-full h-full border-none shadow-sm"
-            variant="outline"
-          >
-            Editar perfil
-          </Button>
-        </Link>
-        <Link href="/new-user" className="w-1/2 aspect-square">
-          <Button
-            className="w-full h-full border-none shadow-sm"
-            variant="outline"
-          >
-            Criar usuário
-          </Button>
-        </Link>
-      </div> */}
     </div>
   );
 };
