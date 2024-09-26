484b7cb82858605ec6674b627b0281e7179be76d
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2024-09-26 15:42:44 -0300

    fix

--- app/(pages)/test/_components/form.tsx
+++ app/(pages)/test/_components/form.tsx
@@ -9,6 +9,19 @@ import { Input } from "@/app/_components/ui/input";
 import { convertBlobUrlToFile } from "@/app/_lib/utils";
 import { uploadImage } from "@/supabase/storage/client";
 import { PlusIcon, XIcon } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/app/_components/ui/select";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "@/app/_components/ui/dialog";
+import NewCategory from "./new-category";
 
 interface Category {
   id: string;
@@ -37,6 +50,7 @@ const CreateProductForm = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [categories, setCategories] = useState<Category[]>([]);
   const [marks, setMarks] = useState<Mark[]>([]);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [files, setFiles] = useState<File[]>([]);
   const [imagesUploaded, setImagesUploaded] = useState(false);
   const [formData, setFormData] = useState<FormData>({
@@ -56,27 +70,50 @@ const CreateProductForm = () => {
     setFormData((prev) => ({ ...prev, [id]: value }));
   };
 
+  // Função para buscar categorias do banco de dados
+  const fetchCategories = async () => {
+    try {
+      const categoriesResponse = await fetch("/api/categories");
+      const categoriesData = await categoriesResponse.json();
+      setCategories(categoriesData);
+    } catch (error) {
+      console.error("Erro ao buscar categorias:", error);
+    }
+  };
+
+  // Função para buscar marcas do banco de dados
+  const fetchMarks = async () => {
+    try {
+      const marksResponse = await fetch("/api/marks");
+      const marksData = await marksResponse.json();
+      setMarks(marksData);
+    } catch (error) {
+      console.error("Erro ao buscar marcas:", error);
+    }
+  };
+
   // Função para buscar categorias e marcas do banco de dados
+  const fetchCategoriesAndMarks = async () => {
+    try {
+      // Usando Promise.all para buscar categorias e marcas simultaneamente
+      const [categoriesData, marksData] = await Promise.all([
+        fetchCategories(),
+        fetchMarks(),
+      ]);
+    } catch (error) {
+      console.error("Erro ao buscar categorias e marcas:", error);
+    }
+  };
+
   useEffect(() => {
-    const fetchCategoriesAndMarks = async () => {
-      try {
-        const [categoriesResponse, marksResponse] = await Promise.all([
-          fetch("/api/categories"),
-          fetch("/api/marks"),
-        ]);
-        const [categoriesData, marksData] = await Promise.all([
-          categoriesResponse.json(),
-          marksResponse.json(),
-        ]);
-        setCategories(categoriesData);
-        setMarks(marksData);
-      } catch (error) {
-        console.error("Erro ao buscar categorias e marcas:", error);
-      }
-    };
     fetchCategoriesAndMarks();
   }, []);
 
+  const handleDialogClose = () => {
+    setIsDialogOpen(false);
+  };
+  
+  // Função de envio do formulário 
   const submitFormData = async () => {
     try {
       setIsLoading(true);
@@ -228,9 +265,9 @@ const CreateProductForm = () => {
             <Label htmlFor="price">Preço</Label>
             <Input
               type="number"
-              placeholder="Digite o preço do produto"
+              placeholder="R$ 0,00"
               id="price"
-              value={formData.price}
+              value={formData.price ? formData.price : "R$ 0,00"}
               onChange={handleInputChange}
             />
           </div>
@@ -246,38 +283,72 @@ const CreateProductForm = () => {
           </div>
         </div>
         {/* Categorias e Marcas */}
-        <div className="flex gap-2">
-          <div className="flex flex-col gap-2 w-full">
-            <Label htmlFor="category">Categoria</Label>
-            <select
-              className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-              id="category"
+        <div className="flex gap-2 w-full">
+          <div className="flex flex-col gap-2 w-1/2">
+            <Label>Categorias</Label>
+            <Select
               value={formData.category}
-              onChange={handleInputChange}
+              onValueChange={(value) =>
+                setFormData((prev) => ({ ...prev, category: value }))
+              }
             >
-              <option value="">Selecione uma categoria</option>
-              {categories.map((category) => (
-                <option key={category.id} value={category.id}>
-                  {category.name}
-                </option>
-              ))}
-            </select>
+              <SelectTrigger className="text-gray-500">
+                <SelectValue placeholder="Selecione uma opção" className="" />
+              </SelectTrigger>
+              <SelectContent className="w-full min-w-[100%]">
+                {categories.map((category) => (
+                  <SelectItem
+                    className="w-full"
+                    key={category.id}
+                    value={category.id}
+                  >
+                    {category.name}
+                  </SelectItem>
+                ))}
+                <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+                  <DialogTrigger className="relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-4 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
+                    <Button className="w-full">Nova categroia</Button>
+                  </DialogTrigger>
+                  <DialogContent className="w-[90%] rounded-md">
+                    <NewCategory
+                      refreshCategories={fetchCategoriesAndMarks}
+                      closeDialog={handleDialogClose}
+                    />
+                  </DialogContent>
+                </Dialog>
+              </SelectContent>
+            </Select>
           </div>
-          <div className="flex flex-col gap-2 w-full">
-            <Label htmlFor="mark">Marca</Label>
-            <select
-              className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-              id="mark"
+          <div className="flex flex-col gap-2 w-1/2">
+            <Label>Marcas</Label>
+            <Select
               value={formData.mark}
-              onChange={handleInputChange}
+              onValueChange={(value) =>
+                setFormData((prev) => ({ ...prev, mark: value }))
+              }
             >
-              <option value="">Selecione uma marca</option>
-              {marks.map((mark) => (
-                <option key={mark.id} value={mark.id}>
-                  {mark.name}
-                </option>
-              ))}
-            </select>
+              <SelectTrigger className="text-gray-500">
+                <SelectValue placeholder="Selecione uma opção" />
+              </SelectTrigger>
+              <SelectContent>
+                {marks.map((mark) => (
+                  <SelectItem key={mark.id} value={mark.id}>
+                    {mark.name}
+                  </SelectItem>
+                ))}
+                <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+                  <DialogTrigger className="relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-4 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
+                    <Button className="w-full">Nova marca</Button>
+                  </DialogTrigger>
+                  <DialogContent className="w-[90%] rounded-md">
+                    <NewCategory
+                      refreshCategories={fetchCategoriesAndMarks}
+                      closeDialog={handleDialogClose}
+                    />
+                  </DialogContent>
+                </Dialog>
+              </SelectContent>
+            </Select>
           </div>
         </div>
         {/* Botão de enviar */}
@@ -286,7 +357,7 @@ const CreateProductForm = () => {
             type="button"
             onClick={handleClickUploadImagesButton}
             disabled={files.length === 0 || isLoading || isPending}
-            className="your-button-class"
+            className="your-button-class w-full"
           >
             {isLoading || isPending ? "Carregando..." : "Criar Produto"}
           </Button>
